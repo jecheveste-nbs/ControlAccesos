7c34fa70e974003323b829598bc5b7ae9d4c4f5e
jecheveste-nbs <jecheveste@nearbysensor.com>
2023-04-04 16:55:53 +0200

    Fin dia 4-4-2023

--- Calendario/Modal/Form_Calendar.php
+++ Calendario/Modal/Form_Calendar.php
@@ -1,29 +1,14 @@
 <div id="idmodal" class="modal">
-    <form class="modal-content animate">
-      <div class="container">
-        <h2>Darse de alta en el sistema </h2>
-            <label for="nombre"><b>Nombre</b></label>
+    <div style="position:relative;">
+        <form class="modal-content animate" style="  position: relative;border:10px solid #73AD21;  ">
+        <div class="container " >
+            <h2>Darse de alta en el sistema </h2>
+            <label class="labeles" for="nombre"><b>Nombre</b></label>
+            <label class="labeles" for="apellido"><b>Apellido(s) </b></label>
+            <label class="labeles" for="grupo"><b>Grupo </b></label>
+            <button type="button" onclick="togglemodal()" class="cancelbtn">Cerrar</button>
             
-            <label for="apellido"><b>Apellido(s) </b></label>
-            <input type="text" placeholder="Introduce tu apellido" name="apellido" required>
-            <label for="grupo"><b>Grupo </b></label>
-            <select name="grupo" id="selector">
-            <?php
-                $iDb=DbGet();
-                $iSql="SELECT IdGrupo,Descripcion FROM nbs_accesos.nsgrupos;";
-
-                $iReg=DbGetReg($iSql,$iDb);
-                if ($iReg!==false){        
-                    while ($iRow = $iReg->fetch_assoc()) {
-                        echo ('<option value="'.$iRow["IdGrupo"].'">'.$iRow["Descripcion"].'</option>');
-                    }
-                }
-                ?>
-            </select>
-            <button type="submit">Enrolarse</button>
         </div>
-        <div class="container" style="background-color:#f1f1f1">
-            <button type="button" onclick="togglemodal()" class="cancelbtn">Cancel</button>
-    </div>
-  </form>
+    </form>
+  </div>
 </div>
